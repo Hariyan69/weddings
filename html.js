a87588353a793f1e7ce100c43060a32e4199dab1
Hariyan69 <153974662+Hariyan69@users.noreply.github.com>
2024-03-01 16:19:00 +0530

    Add files via upload

---
+++ html.js
@@ -0,0 +1,65 @@
+const form = document.querySelector("#form");
+const names = document.querySelector("#names");
+const email = document.querySelector("#email")
+const message = document.querySelector("#message");
+
+
+form.addEventListener("submit", (e) => {
+    validateInputs();
+    
+    function validateInputs() {
+      const nameVal = names.value.trim();
+      const emailVal = email.value.trim();
+      const messageVal = message.value.trim();
+      var validateEmail = /^([a-z0-9_\-\.])+\@([a-z0-9_\-\.])+\.([a-z]{2,})$/
+
+      if (nameVal === "") {
+        e.preventDefault();
+        setError(names, "Name is required");
+      } 
+      else {
+        setSuccess(names);
+      }
+
+      if (emailVal === "") {
+        e.preventDefault();
+        setError(email, "Email is required");
+      } else if (!emailVal.match(validateEmail)) {
+        e.preventDefault();
+        setError(email, "Please enter a valid email");
+      }
+       else {
+        setSuccess(email);
+      }
+      if(messageVal === ""){
+        e.preventDefault();
+        setError(message, "Please enter your comments");
+      }
+      else {
+        setSuccess(message);
+      }
+    }
+  });
+
+  function setError(element, message) {
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector('.error');
+
+    errorElement.innerText = message;
+    inputGroup.classList.add('error');
+    inputGroup.classList.remove('success');
+  }
+
+  function setSuccess(element) {
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector('.error');
+
+    errorElement.innerText = '';
+    inputGroup.classList.add('success');
+    inputGroup.classList.remove('error');
+ }
+
+
+
+
+
